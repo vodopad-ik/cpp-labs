ec9e47365a51efa3bcc4ad0870ac1ab39466b6b9
vodopad_ik <byskalovad@gmail.com>
2025-09-18 16:47:35 +0300

    SONAR change issues 12,readme.md

--- LAB_2/utils.hpp
+++ LAB_2/utils.hpp
@@ -2,7 +2,7 @@
 #include "array.hpp"
 #include <string>
 
-int input(const std::string& message = "");
-int positiveInput(const std::string& message = "");
-int placeElement(Array& item, int index);
-void print(const Array& item);
+int input(const std::string &message = "");
+int positiveInput(const std::string &message = "");
+int placeElement(Array &item, int index);
+void print(const Array &item);
