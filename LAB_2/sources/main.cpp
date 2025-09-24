22f4c921da501e1acf4d09e3cbfa954c432dc0a1
vodopad_ik <byskalovad@gmail.com>
2025-09-24 19:16:57 +0300

    sonar changes

--- LAB_2/sources/main.cpp
+++ LAB_2/sources/main.cpp
@@ -37,16 +37,13 @@ int main() {
         print(item1);
       else if (iteration == 1)
         print(item2);
-      else if (iteration >= 2 && item3.getLength()) {
-        print(item3);
-        return 0;
-      }
-      else if (iteration >= 2) 
+      else if (iteration >= 2)
         print(item3);
       break;
     case 3:
-      if (iteration++ >= 2)
-        cout << "Выберите один из вариантов, представленных в меню!!!\n";
+      if (iteration >= 2)
+        item3 = item2 | item1;
+      iteration++;
       break;
     default:
       cout << "Выберите один из вариантов, представленных в меню!!!\n";
