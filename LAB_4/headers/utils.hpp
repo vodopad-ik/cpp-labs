d9776af7e327ea98dae63bba94ad7da9c3234a29
vodopad_ik <byskalovad@gmail.com>
2025-10-07 12:07:27 +0300

    sonar change

--- LAB_4/headers/utils.hpp
+++ LAB_4/headers/utils.hpp
@@ -4,7 +4,7 @@
 class Utils {
 public:
   static void clearScreen();
-  static int inputInteger(const std::string &message = "");
+  static int inputInteger(const std::string &message = ""); 
   static double inputDouble(const std::string &message = "");
   static int
   positiveInputInteger(const std::string &message = "", int max_value = 0,
